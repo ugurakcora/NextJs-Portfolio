79c38a6ec009bf325fd4236b3e1b263910c17d03
ugurakcora <uakcora97@gmail.com>
2023-08-29 21:13:56 +0300

    header init

---
+++ components/header.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header className='z-[999] relative'>
+      <div className='fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full'></div>
+    </header>
+  )
+}
