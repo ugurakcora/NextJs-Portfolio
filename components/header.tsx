a932c89e62abfda86b599f8aa09006fd64e23a74
ugurakcora <uakcora97@gmail.com>
2023-08-30 15:23:24 +0300

    Updated

--- components/header.tsx
+++ components/header.tsx
@@ -7,7 +7,7 @@ import { links } from '@/lib/data'
 export default function Header() {
   return (
     <header className='z-[999] relative'>
-      <motion.div className='fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full'
+      <motion.div className='fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75'
         initial={{ y: -100, x: "-50%", opacity: 0 }}
         animate={{ y: 0, x: "-50%", opacity: 1 }}
       ></motion.div>
@@ -15,11 +15,11 @@ export default function Header() {
         <ul className='flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5'>
           {
             links.map((link) => (
-              <motion.li className='h-3/4 w-full flex items-center justify-center px-3 py-3 hover:text-gray-950 transition:' key={link.hash} 
+              <motion.li className='h-3/4 flex items-center justify-center relative hover:text-gray-950 transition:' key={link.hash} 
                 initial={{ y: -100, opacity: 0 }}
                 animate={{ y: 0, opacity: 1 }}
               >
-                <Link href={link.hash}>
+                <Link href={link.hash} className='flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300 text-gray-950 dark:text-gray-200'>
                   {link.name}
                 </Link>
               </motion.li>
--- components/intro.tsx
+++ components/intro.tsx
@@ -3,6 +3,10 @@ import React from 'react'
 import Image from 'next/image'
 import heroImg from "@/public/hero.jpg";
 import { motion } from 'framer-motion';
+import Link from 'next/link';
+import { BsArrowRight, BsLinkedin } from 'react-icons/bs';
+import { HiDownload } from 'react-icons/hi';
+import { FaGithubSquare } from 'react-icons/fa';
 
 function getCurrentYear() {
   const currentYear = new Date().getFullYear() - 2019;
@@ -38,13 +42,32 @@ export default function Intro() {
           >👋</motion.span>
         </div>
       </div>
-      <p className='mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl'>
+      <motion.p className='mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl'
+        initial={{ y: 100, opacity: 0 }}
+        animate={{ y: 100, opacity: 1 }}
+        >
         <span className='font-bold'>Hello, I'm Uğur Akçora.</span> I'm a{' '}
         <span className='font-bold'>Frontend Developer</span> with{' '}
         <span className='font-bold'>{currentYear} Years</span> of experience. I enjoy building
         <span className='italic'>sites & apps</span>. My focus is{' '}
         <span className='underline'>React (Next.js)</span>.
-      </p>
+      </motion.p>
+
+      <motion.div className='flex flex-col items-center justify-center sm:flex-row gap-2 px-4 text-lg font-medium'
+        initial={{ y: 100, opacity: 0 }}
+        animate={{ y: 100, opacity: 1 }}
+        transition={{ delay: 0.1 }}
+        >
+        <Link href='#contact' className='group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 outline-none rounded-full focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition'>Contact Me Here <BsArrowRight className='opacity-70 group-hover:translate-x-1 transition' /></Link>
+
+        <a className='group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110  active:scale-105 transition cursor-pointer border border-black/10' href='/CV.pdf' download>Download CV <HiDownload className='opacity-60 group-hover:translate-y-1 transition'/></a>
+
+
+        <a className='bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full focus:scale-110 hover:scale-110  active:scale-105 transition cursor-pointer border border-black/10'> <BsLinkedin /> </a>
+
+
+        <a className='bg-white p-3.5 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15]  active:scale-105 transition cursor-pointer border border-black/10'> <FaGithubSquare /> </a>
+      </motion.div>
     </section>
   )
 }
