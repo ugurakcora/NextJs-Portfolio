6f797f8d3d86a588d633a98105c02b9dfd3f684b
ugurakcora <uakcora97@gmail.com>
2023-08-31 13:35:05 +0300

    added about

---
+++ components/about.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <section>
+      <h2>About Me</h2>
+      <p className="mb-3">
+        After graduating with a degree in{" "}
+        <span className="font-medium">Computer Programing</span>, I decided to pursue my
+        passion for programming. I enrolled in a coding bootcamp and learned{" "}
+        <span className="font-medium">frontend web development</span>.{" "}
+        <span className="italic">My favorite part of programming</span> is the
+        problem-solving aspect. I <span className="underline">love</span> the
+        feeling of finally figuring out a solution to a problem. My core stack
+        is{" "}
+        <span className="font-medium">
+          React, Next.js
+        </span>
+        . I am also familiar with TypeScript. I am always looking to
+        learn new technologies. I am currently looking for a{" "}
+        <span className="font-medium">full-time position</span> as a software
+        developer.
+      </p>
+
+      <p>
+        <span className="italic">When I'm not coding</span>, I enjoy playing
+        video games, watching movies, and playing with my cat. I also enjoy{" "}
+        <span className="font-medium">learning new things</span>. I am currently
+        learning about{" "}
+        <span className="font-medium">history and philosophy</span>. I'm also
+        learning how to play the guitar & piano.
+      </p>
+    </section>
+  )
+}
--- components/intro.tsx
+++ components/intro.tsx
@@ -42,7 +42,7 @@ export default function Intro() {
           >👋</motion.span>
         </div>
       </div>
-      <motion.p className='mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl'
+      <motion.h1 className='mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl'
         initial={{ y: 100, opacity: 0 }}
         animate={{ y: 100, opacity: 1 }}
         >
@@ -51,7 +51,7 @@ export default function Intro() {
         <span className='font-bold'>{currentYear} Years</span> of experience. I enjoy building
         <span className='italic'>sites & apps</span>. My focus is{' '}
         <span className='underline'>React (Next.js)</span>.
-      </motion.p>
+      </motion.h1>
 
       <motion.div className='flex flex-col items-center justify-center sm:flex-row gap-2 px-4 text-lg font-medium'
         initial={{ y: 100, opacity: 0 }}
