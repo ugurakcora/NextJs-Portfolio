16b5fbb0945d2504ae6c4ce94012f097ed6ddb97
ugurakcora <uakcora97@gmail.com>
2023-08-31 13:44:48 +0300

    Updated dinamic title

--- components/about.tsx
+++ components/about.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import SectionHeading from './section-heading'
 
 export default function About() {
   return (
-    <section>
-      <h2>About Me</h2>
+    <section className='mb-28 max-w-[45rem] text-center leading-8 sm:mb-40'>
+      <SectionHeading>About Me</SectionHeading>
       <p className="mb-3">
         After graduating with a degree in{" "}
         <span className="font-medium">Computer Programing</span>, I decided to pursue my
---
+++ components/section-heading.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+type SectionHeadingProps = { 
+  children: React.ReactNode
+};
+
+export default function SectionHeading({
+  children}: SectionHeadingProps) {
+  return (
+    <h2 className='text-3xl font-medium capitalize mb-8'>{children}</h2>
+  )
+}
