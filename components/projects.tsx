03a564cab451a68cc9cd35ffb6c943107925b0a8
ugurakcora <uakcora97@gmail.com>
2023-09-01 13:31:43 +0300

    init projects

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from 'next/image'
 import SectionHeading from './section-heading'
 import { projectsData } from '@/lib/data'
 
@@ -19,8 +20,21 @@ export default function Projects() {
   )
 }
 
-type ProjectProps = (typeof projectsData)[0];
+type ProjectProps = (typeof projectsData)[number];
 
 function Project({title, description, tags, imageUrl} : ProjectProps){
-  return <div>{title}</div>
+  return (
+    <section>
+      <h3>{ title }</h3>
+      <p>{ description }</p>
+      <ul>
+        {
+          tags.map((tag, index) => (
+            <li key={index}>{tag}</li>
+          ))
+        } 
+      </ul>
+      <Image src={imageUrl} alt={title} quality={95}/>
+    </section>
+  ) 
 }
