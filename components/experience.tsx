f807dd76a166e40c25ef38e7d83d9466931c664a
ugurakcora <uakcora97@gmail.com>
2023-09-03 18:27:34 +0300

    Added Experience Title

--- components/experience.tsx
+++ components/experience.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import SectionHeading from './section-heading'
 
 export default function Experience() {
   return (
-    <div>E</div>
+    <section>
+      <SectionHeading>My Experience</SectionHeading>
+    </section>
   )
 }
