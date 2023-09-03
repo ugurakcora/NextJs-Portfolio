aaac4be7cec61281c17d0270e43ac2c766acd3b8
ugurakcora <uakcora97@gmail.com>
2023-09-03 17:28:04 +0300

    Added map

--- components/skills.tsx
+++ components/skills.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import SectionHeading from './section-heading'
+import { skillsData } from '@/lib/data'
 
 export default function Skills() {
   return (
     <section>
         <SectionHeading>My Skills</SectionHeading>
+        <ul>
+          {
+            skillsData.map((skill, index) => (
+              <li key={index} className="flex flex-col items-center justify-center">{ skill }</li>
+            ))
+          }
+        </ul>
     </section>
   )
 }
