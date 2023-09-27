ac067276a76f26c1b9f87382ec98cbdb876c5ecb
ugurakcora <uakcora97@gmail.com>
2023-09-27 19:22:21 +0300

    updated cv

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
       <SectionDivider />
       <About />
       <Projects />
-      <Github />
+      {/* <Github /> */}
       <Skills />
       <Experience />
       <Contact />
--- components/github.tsx
+++ components/github.tsx
@@ -6,47 +6,60 @@ import SectionHeading from './section-heading';
 import { BsStar, BsEye } from 'react-icons/bs'
 
 const GitHub = () => {
-  const [data, setData] = useState([]);
+interface RepoData {
+  repo: string;
+  link: string;
+  language: string;
+  stars: number;
+  forks: number;
+}
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await getPinnedRepos('ugurakcora');
-        setData(response);
-      } catch (error) {
-        console.error(error);
-      }
-    };
+const [data, setData] = useState<RepoData[]>([]);
 
-    fetchData();
-  }, []);
+useEffect(() => {
+  async function fetchData() {
+    const response = await fetch('https://api.github.com/users/ugurakcora/repos');
+    const json = await response.json();
+    setData(json.map((repo: any) => ({
+      repo: repo.name,
+      link: repo.html_url,
+      language: repo.language,
+      stars: repo.stargazers_count,
+      forks: repo.forks_count,
+    })));
+  }
+  fetchData();
+}, []);
+  console.log(data)
 
   return (
     <section className='scroll-mt-28 mb-28'>
       <SectionHeading>Github</SectionHeading>
       <div className="flex gap-6 flex-wrap items-center justify-center">
-      {data.map((repo) => (
-        <div className="group h-full w-1/2" key={repo.repo}>
-          <div className="card flex items-center flex-col h-full w-full p-4 md:p-6 sm:w-1/2">
-              <a href={repo.link} target="_blank" rel="noreferrer" className="font-semibold lg:text-lg flex items-center space-x-2">
-                <span className="truncate">{repo.repo}</span></a>
-                <p className="text-sm mt-2 text-zinc-500">dummy text</p>
-                <div className="flex items-center mt-auto">
-                  <div className="text-sm inline-flex items-center gap-1 mr-4">
-                    {/* icon */}
-                    <span>{repo.language}</span>
-                  </div>
-                  <div className="text-sm inline-flex items-center gap-1 mr-2">
-                    <BsStar />
-                    <span>{repo.stars}</span>
-                  </div>
-                  <div className="text-sm inline-flex items-center gap-1 mr-2">
-                    <BsEye />
-                    <span>{repo.forks}</span>
-                  </div>
-              </div>
+      {data.map((item) => (
+        item.stars > 0 ? (
+          <div className="group h-full w-1/2" key={item.repo}>
+            <div className="card flex items-center flex-col h-full w-full p-4 md:p-6 sm:w-1/2">
+                <a href={item.link} target="_blank" rel="noreferrer" className="font-semibold lg:text-lg flex items-center space-x-2">
+                  <span className="truncate">{item.repo}</span></a>
+                  <p className="text-sm mt-2 text-zinc-500">dummy text</p>
+                  <div className="flex items-center mt-auto">
+                    <div className="text-sm inline-flex items-center gap-1 mr-4">
+                      {/* icon */}
+                      <span>{item.language}</span>
+                    </div>
+                    <div className="text-sm inline-flex items-center gap-1 mr-2">
+                      <BsStar />
+                      <span>{item.stars}</span>
+                    </div>
+                    <div className="text-sm inline-flex items-center gap-1 mr-2">
+                      <BsEye />
+                      <span>{item.forks}</span>
+                    </div>
+                </div>
+            </div>
           </div>
-        </div>
+        ) : (null)
       ))}
       </div>
 
