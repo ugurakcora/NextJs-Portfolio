ffbf845084343c1215720e7ad1d87b9e5280b89f
ugurakcora <uakcora97@gmail.com>
2023-09-29 00:37:30 +0300

    updated experiencesData

--- lib/data.ts
+++ lib/data.ts
@@ -45,7 +45,7 @@ export const experiencesData = [
     position: "Front-end Developer",
     location: "İstanbul",
     description:
-      "I took part in the development of the front end of the CRM product using React. I coded my profile page.",
+      "As a React Developer at Next4Biz, I played a pivotal role in enhancing the CRM product by focusing on the profile page. Leveraging my expertise in React, I meticulously crafted the profile page, optimizing its design and functionality to ensure a seamless user experience. Collaborating closely with the team, I incorporated innovative features and improvements, meeting both client specifications and user expectations. This experience honed my skills in React development and expanded my understanding of creating user-centric solutions within a collaborative and dynamic work environment.",
     icon: React.createElement(FaReact),
     date: "2023",
   },
@@ -54,7 +54,7 @@ export const experiencesData = [
     position: "Front-end Developer",
     location: "İstanbul",
     description:
-      "I developed the frontends of e-commerce sites",
+      "During my tenure at Akinon, I excelled as a Frontend Developer, specializing in crafting the front-end of diverse e-commerce websites. My skill set encompassed Django, Jinja2, JavaScript, jQuery, Less, and Scss. I actively contributed to notable projects including Converse, Houseofsuperstep, Superstep, Gant, Occasion, and Kooples. Working in collaboration with a colleague, I co-developed the Gant project from the ground up. At Kooples, I not only contributed to enhancements but also coded the user-friendly returns section. In the case of Superstep, I integrated Masterpass and implemented various enhancements. Across all projects, I focused on both development and bug fixes, continually striving to elevate the user experience and functionality.",
     icon: React.createElement(CgWorkAlt),
     date: "2021",
   },
@@ -62,8 +62,7 @@ export const experiencesData = [
     title: "Pixelplus",
     position: "Front-end Developer",
     location: "İstanbul",
-    description:
-      "I coded the frontends of e-commerce and corporate companies",
+    description: "During my tenure at PixelPlus, I thrived as a Frontend Developer, taking on the responsibility of solo development for web projects including Uzman Sosyal Güvenlik, Vellteks, Form Sünger, Boyser, and Boytrans. Notably, I built these websites from the ground up and seamlessly integrated CMS systems. In addition, I contributed to bug fixes and enhancements for AmericanExpress and Tüyap websites. One of my key approaches was adhering to a 'mobile-first' development methodology, ensuring optimal performance and user experience across devices. Leveraging technologies like Pug.js, JavaScript, jQuery, BEM (Block Element Modifier), and SCSS (Sass), I crafted responsive and visually appealing interfaces. This experience refined my ability to tackle challenges head-on and work effectively both independently and within a team. I'm driven by a passion for creating exceptional web solutions that resonate with users and drive business success.",
     icon: React.createElement(CgWorkAlt),
     date: "2020",
   },
@@ -71,8 +70,7 @@ export const experiencesData = [
     title: "Niceye",
     position: "Front-end Developer",
     location: "İstanbul",
-    description:
-      "I coded Pfizer's drug package insert sites.",
+    description: "As a Frontend Developer at NicEye Group, I played a pivotal role in the coding and enhancement of pharmaceutical prospectus websites and corporate sites for two of the world's leading healthcare companies, Pfizer and MSD. Notable contributions include the development of websites for these major pharmaceutical firms, such as hoevoorkomjetbe, enbrelpfizer, romatolojiatlasi, sanslicarsamba, and msd, msdsizinle. I utilized a skill set encompassing Html, Css, Scss, Bootstrap4, JavaScript, and JQuery to create visually appealing and highly functional websites. My approach involved ensuring an optimal user experience and seamless functionality across various devices. This experience honed my ability to work in dynamic, results-oriented environments and further developed my passion for crafting impactful digital solutions.",
     icon: React.createElement(CgWorkAlt),
     date: "2019",
   },
